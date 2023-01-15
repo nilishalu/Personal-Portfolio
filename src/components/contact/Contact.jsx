3f019ca9a66a9bf73274ce87e2ab5ac4fe5f5644
nilishalu <shalini.c.e.ganu@gmail.com>
2023-01-15 16:32:51 +0530

    add contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,8 +4,16 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Get it touch with me!</h1>
+    <div className='contact'>
+      <h1>Connect with Me</h1>
+
+      <form action="">
+        <input type="text" placeholder='Name'/>
+        <input type="text" placeholder='Email'/>
+        <input type="text" placeholder='Subject/Purpose'/>
+        <input type="text" placeholder='Message'/>
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
